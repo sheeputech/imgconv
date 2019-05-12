af3f02371f05a387cd16444d1d86850bb89d47c8
Kazumasa <p.m.byzance0817@gmail.com>
2019-05-12 21:09:33 +0900

    Functionalized standard outputs to stdlog() / Functionalized main operations to main()

--- imgconv.py
+++ imgconv.py
@@ -6,61 +6,79 @@ from PIL import Image
 from cairosvg import svg2png
 from tqdm import tqdm
 
-if __name__ == '__main__':
-    print('Image converter start.')
+LOG_INFO = 0
+LOG_ERROR = 1
+LOG_INTERRUPT = 2
+
+
+def stdlog(type, message):
+    if type == 0:
+        print('| info: {message}'.format(message=message))
+    elif type == 1:
+        print('| error: {message}'.format(message=message))
+    elif type == 2:
+        print('| interrupt: {message}'.format(message=message))
+
+
+def main():
+    stdlog(LOG_INFO, 'Image Converter start running.')
 
     # Setup tkinter
     root = tk.Tk()
     root.withdraw()
 
     # Converted files
-    print('1. Choose PNG files converted.')
+    stdlog(LOG_INFO, 'Choose PNG files converted.')
     f_types = [('SVG', '*.svg')]
     init_dir = 'C:\\Users\\kzms9\\Pictures'
     in_files = filedialog.askopenfilenames(filetypes=f_types,
                                            initialdir=init_dir)
 
     if not in_files:
-        print('Image converter interrupted.')
+        stdlog(LOG_INTERRUPT, 'Image converter interrupted.')
         sys.exit()
 
-    print('Chosen files are...')
+    stdlog(LOG_INFO, 'Chosen files are...')
     for i, f in enumerate(in_files):
         print('{index}. {filename}'.format(index=i + 1, filename=f))
 
     # Output directory
-    print('2. Choose output directory.')
+    stdlog(LOG_INFO, 'Choose output directory.')
     out_dir = filedialog.askdirectory(initialdir=init_dir)
 
     if not out_dir:
-        print('Image converter was interrupted.')
+        stdlog(LOG_ERROR, 'Image converter was interrupted.')
         sys.exit()
 
-    print('Output directory is : {dir}'.format(dir=out_dir))
+    stdlog(LOG_INFO, 'Output directory is : {dir}'.format(dir=out_dir))
 
     png_files = [
         '{out_dir}/{filename}.png'.format(out_dir=out_dir, filename=f.rsplit('/', 1)[1].split('.')[0]) for f in in_files]
     ico_files = ['{path}.ico'.format(path=f.rsplit('.')[0]) for f in png_files]
 
-    print('Converting from SVG to PNG')
+    stdlog(LOG_INFO, 'Converting from SVG to PNG')
     for f, p in tqdm(list(zip(in_files, png_files))):
         try:
             svg2png(
                 url=f, write_to=p, output_width=512, output_height=512, scale=2.0)
         except IOError:
-            print('Failed to convert SVG to PNG:', f)
+            stdlog(LOG_ERROR, 'Failed to convert SVG to PNG: {f}'.format(f=f))
 
-    print('Converting from PNG to ICO')
+    stdlog(LOG_INFO, 'Converting from PNG to ICO')
     for p, i in tqdm(list(zip(png_files, ico_files))):
         try:
             img = Image.open(p)
             img.save(i, "ICO")
         except IOError:
-            print('Failed to convert PNG to ICO:', f)
+            stdlog(LOG_ERROR, 'Failed to convert PNG to ICO: {f}'.format(f=f))
 
-    print('Removing temporary PNG files.')
+    stdlog(LOG_INFO, 'Removing temporary PNG files.')
     for f in png_files:
         if os.path.isfile(f):
             os.remove(f)
 
-    print('Image conversion was successfully completed.')
+    stdlog(LOG_INFO, 'Image conversion was successfully completed.')
+
+
+if __name__ == '__main__':
+    main()
