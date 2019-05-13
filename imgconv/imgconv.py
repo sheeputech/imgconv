15013946be37b8a2f1823617f6c3eda95f259b63
Kazumasa <p.m.byzance0817@gmail.com>
2019-05-13 15:39:03 +0900

    Fix error

--- imgconv/imgconv.py
+++ imgconv/imgconv.py
@@ -20,14 +20,11 @@ def stdlog(type, message):
         print('| interrupt:', message)
 
 
-@click.command(
-    help='Image Converter',
-    context_settings=dict(help_option_names=['-h', '--help']),
-)
-@click.option('-i', '--interactive', is_flag=True, help='start interactive image converter')
-@click.option('-f', '--file_paths', 'files', required=False, type=str, help='path of the file you want to convert')
-@click.option('-d', '--dest_dir', required=False, type=str, help='path of destination directory')
-@click.option('-t', '--output_type', 'out_type', required=True, type=click.Choice(['JPG', 'PNG', 'ICO']), default='PNG')
+@click.command(help='Image Converter', context_settings=dict(help_option_names=['-h', '--help']),)
+@click.option('-i', '--interactive', is_flag=True, help='Start interactive image converter.')
+@click.option('-f', '--file_paths', 'files', required=False, type=str, help='Path of files you want to convert.')
+@click.option('-d', '--dest_dir', required=False, type=str, help='Path of destination directory.')
+@click.option('-t', '--output_type', 'out_type', required=False, type=click.Choice(['JPEG', 'PNG', 'ICO']), default='PNG', help='File format to which the target images converted.', show_default=True)
 @click.pass_context
 def main(ctx, interactive, files, dest_dir, out_type):
     # show help by default
@@ -46,7 +43,6 @@ def main(ctx, interactive, files, dest_dir, out_type):
         files = filedialog.askopenfilenames(
             filetypes=[('image files', ('.svg', '.jpg', '.png'))]
         )
-        separated = [f.rsplit('/', 1)[1].split('.') for f in files]
     if len(files) == 0:
         stdlog(LOG_INTERRUPT, 'Image converter interrupted.')
         ctx.exit()
@@ -64,16 +60,23 @@ def main(ctx, interactive, files, dest_dir, out_type):
         ctx.exit()
     stdlog(LOG_INFO, 'Destination directory is : {dir}'.format(dir=dest_dir))
 
+    # set format
+    out_format = 'png'
+    if out_type == 'JPEG':
+        out_format = 'jpg'
+    elif out_type == 'ICO':
+        out_format = 'ico'
+
     # convert images
     stdlog(LOG_INFO, 'Converting images...')
-    for f, s in tqdm(zip(files, separated)):
+    separated = [f.rsplit('/', 1)[1].split('.') for f in files]
+    for f, s in tqdm(list(zip(files, separated))):
         filename = s[0]
         ext = s[1]
-        path = '{dir}/{file}.{ext}'.format(dir=dest_dir,
-                                           file=filename, ext=ext)
+        path = '{}/{}.{}'.format(dest_dir, filename, out_format)
+
         if ext == 'svg':
-            path_png = '{dir}/{file}.{ext}'.format(
-                dir=dest_dir, file=filename, ext='png')
+            path_png = '{}/{}.{}'.format(dest_dir, filename, 'png')
             try:
                 svg2png(url=f, write_to=path_png,
                         output_width=512, output_height=512, scale=2.0)
@@ -82,16 +85,16 @@ def main(ctx, interactive, files, dest_dir, out_type):
             if out_type != 'PNG':
                 try:
                     img = Image.open(path_png)
-                    img.save(path, out_type)
+                    img.save(path, out_format)
                     os.remove(path_png)
                 except IOError:
                     stdlog(LOG_ERROR, 'Failed to convert image: {}'.format(f))
         else:
             try:
                 img = Image.open(f)
-                img.save(path, out_type)
+                img.save(path, out_format)
             except IOError:
                 stdlog(LOG_ERROR, 'Failed to convert image: {}'.format(f))
 
     # end
-    stdlog(LOG_INFO, 'Image conversion was successfully completed.')
+    stdlog(LOG_INFO, 'Image conversion finished.')
